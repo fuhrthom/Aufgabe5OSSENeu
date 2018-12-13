2a19b9650f092579f589d1e2115cb8fc5737a1be
Thomas Fuhr <thomas.fuhr@hs-furtwangen.de>
2018-12-13 15:44:08 +0100

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[]) {
+        Scanner eingabe = new Scanner(System.in);
+        String ergebnis = eingabe.nextLine();
+        System.out.println(ergebnis.toUpperCase());
+    }
+}
